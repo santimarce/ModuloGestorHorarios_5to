93600a04ad473aa59950c1552a7010600d3a5772
StefySalazar25 <salazarlstefanym@gmail.com>
2024-03-13 22:47:52 -0500

    formregistro

--- src/app/components/forms/formregister/formregister.component.ts
+++ src/app/components/forms/formregister/formregister.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formregister',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './formregister.component.css'
 })
 export class FormregisterComponent {
+  @Input()
+  title!: string;
 
+  formreg: FormGroup = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    apellido: new FormControl('', Validators.required),
+    fechanacimiento: new FormControl('', Validators.required),
+    contacto: new FormControl('', Validators.required),
+    direccion: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    contrasena: new FormControl('', Validators.required),
+    jornada: new FormControl('', Validators.required),
+    nivel: new FormControl('', Validators.required),
+    paralelo: new FormControl('', Validators.required),
+    carrera: new FormControl('', Validators.required),
+  });
 }
