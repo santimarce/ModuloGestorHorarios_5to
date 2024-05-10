9db681668f91b73259a9343b38833a418bb12169
StefySalazar25 <salazarlstefanym@gmail.com>
2024-05-09 23:35:00 -0500

    stefany-imagen

--- src/app/components/basics/navbar/navbar.component.ts
+++ src/app/components/basics/navbar/navbar.component.ts
@@ -1,33 +1,30 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import {Component, inject, Input} from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { map, Observable, shareReplay } from 'rxjs';
 
-
-
-/**
- * @title Tab group with aligned labels
- */
 @Component({
   selector: 'app-navbar-component',
   templateUrl: 'navbar.component.html',
-  styleUrl: 'navbar.component.css',
+  styleUrls: ['navbar.component.css'],
   standalone: false,
- 
 })
 export class NavbarComponent {
   @Input() navItems!: string[]; // Array de cadenas para los elementos del navbar
   @Input() navbarColor!: string; // Color del navbar
-  @Input() navbarRoutes!: string[]; 
+  @Input() navbarRoutes!: string[];
   title = 'Sistema de Gestión de Horarios ITS YAVIRAC';
-  condition: string = "default"; 
-  changeCondition(name:string) {
+  condition: string = 'default';
+
+  changeCondition(name: string) {
     this.condition = name;
   }
-  private breakpointObserver = inject(BreakpointObserver);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 }
--- src/app/views/dashboardteacher/dashboardteacher.component.ts
+++ src/app/views/dashboardteacher/dashboardteacher.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboardteacher',
   templateUrl: './dashboardteacher.component.html',
-  styleUrl: './dashboardteacher.component.css'
+  styleUrls: ['./dashboardteacher.component.css']
 })
 export class DashboardteacherComponent {
-  nombre = ['Ver Horario', 'Salir'];
+  nombre = ['Docentes', 'Agregar Estudiante', 'Modificar Horarios'];
+
 }
